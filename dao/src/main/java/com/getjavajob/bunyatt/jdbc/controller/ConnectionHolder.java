d169e214f0a004e3429360e92e1dd2905b870770
t.bunyat <t.bunyat@tinkoff.ru>
2018-02-16 21:32:00 +0300

    16.02

--- dao/src/main/java/com/getjavajob/bunyatt/jdbc/controller/ConnectionHolder.java
+++ dao/src/main/java/com/getjavajob/bunyatt/jdbc/controller/ConnectionHolder.java
@@ -1,8 +1,14 @@
 package com.getjavajob.bunyatt.jdbc.controller;
 
+import javax.sql.DataSource;
+
 public class ConnectionHolder {
 
     //todo Это пуш коннешнов.
     //todo Узнать есть ли уже реализация и не выдумывать велик
     //todo
+
+    public static void main(String[] args) {
+
+    }
 }
